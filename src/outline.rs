4fc9ad4316e5ec135974738fb3e395a573b02384
Brian Savage <savage@uri.edu>
2019-01-03 19:23:56 -0500

    Added doc to outline.rs:RasterizerOutline

--- src/outline.rs
+++ src/outline.rs
@@ -29,6 +29,25 @@ impl From<Subpixel> for i64 {
 ///
 /// The rendering is directly attached and drawing is done immediately.
 ///
+///     use agg::{Pixfmt,Rgb8,Rgba8,RenderingBase,SetColor};
+///     use agg::{RendererPrimatives,RasterizerOutline};
+///     use agg::PixelData;
+///     let pix = Pixfmt::<Rgb8>::new(100,100);
+///     let mut ren_base = agg::RenderingBase::new(pix);
+///     ren_base.clear( Rgba8::new(255, 255, 255, 255) );
+///
+///     let mut ren = RendererPrimatives::with_base(&mut ren_base);
+///     ren.line_color(agg::Rgba8::new(0,0,0,255));
+///
+///     let mut path = agg::PathStorage::new();
+///     path.move_to(10.0, 10.0);
+///     path.line_to(50.0, 90.0);
+///     path.line_to(90.0, 10.0);
+///
+///     let mut ras = RasterizerOutline::with_primative(&mut ren);
+///     ras.add_path(&path);
+///     agg::ppm::write_ppm(&ren_base.pixeldata(), 100,100,
+///                         "primative.ppm").unwrap();
 pub struct RasterizerOutline<'a,T> where T: PixelDraw {
     ren: &'a mut RendererPrimatives<'a,T>,
     start_x: Subpixel,
