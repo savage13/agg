805c0d3f841df967eb143d2b1f0f25737358191f
Brian Savage <savage@uri.edu>
2019-01-09 07:13:27 -0500

    Add files to the gallery, needs documentation

--- src/gallery.rs
+++ src/gallery.rs
@@ -1,6 +1,60 @@
 
 //! Gallery of images produced using agg
 //!
+//! <img src="https://raw.githubusercontent.com/savage13/agg/master/images/lion.png" height="150"/>
+//!
+
+//! ![test 01](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_01.png)
+//!
+//! ![test 02](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_02.png)
+//!
+//! ![test 03](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_03.png)
+//!
+//! ![test 04](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_04.png)
+//!
+//! ![test 05](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_05.png)
+//!
+//! ![test 11](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_11.png)
+//!
+//! ![test 12](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_12.png)
+//!
+//! ![test 13](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_13.png)
+//!
+//! ![test 14](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_14.png)
+//!
+//! ![test 15](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_15.png)
+//!
+//! ![test 16](https://raw.githubusercontent.com/savage13/agg/master/images/agg_test_16.png)
+//!
+//! ![componenet rendering alpha = 0](https://raw.githubusercontent.com/savage13/agg/master/images/component_rendering_000.png)
+//!
+//! ![component rendering alpha = 128](https://raw.githubusercontent.com/savage13/agg/master/images/component_rendering_128.png)
+//!
+//! ![component rendering alpha = 255](https://raw.githubusercontent.com/savage13/agg/master/images/component_rendering_255.png)
+//!
 //! ![lion](https://raw.githubusercontent.com/savage13/agg/master/images/lion.png)
-//! 
-//! Done
+//!
+//! ![lion clockwise](https://raw.githubusercontent.com/savage13/agg/master/images/lion_cw.png)
+//!
+//! ![lion clockwise AA](https://raw.githubusercontent.com/savage13/agg/master/images/lion_cw_aa.png)
+//!
+//! ![lion clockwise AA SRGBA](https://raw.githubusercontent.com/savage13/agg/master/images/lion_cw_aa_srgba.png)
+//!
+//! ![lion outline](https://raw.githubusercontent.com/savage13/agg/master/images/lion_outline.png)
+//!
+//! ![lion outline width 1](https://raw.githubusercontent.com/savage13/agg/master/images/lion_outline_width1.png)
+//!
+//! ![little black triangle](https://raw.githubusercontent.com/savage13/agg/master/images/little_black_triangle.png)
+//!
+//! ![outline aa](https://raw.githubusercontent.com/savage13/agg/master/images/outline_aa.png)
+//!
+//! ![rasterizers](https://raw.githubusercontent.com/savage13/agg/master/images/rasterizers.png)
+//!
+//! ![rasterizers2](https://raw.githubusercontent.com/savage13/agg/master/images/rasterizers2.png)
+//!
+//! ![rasterizer2 premultiplied](https://raw.githubusercontent.com/savage13/agg/master/images/rasterizers2_pre.png)
+//!
+//! ![rasterizer_gamm](https://raw.githubusercontent.com/savage13/agg/master/images/rasterizers_gamma.png)
+//!
+//! ![rounded rect](https://raw.githubusercontent.com/savage13/agg/master/images/rounded_rect.png)
+//!
